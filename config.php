241e9774907e3c65be828060066fa4750c40336d
VotreNomGitHub <VotreEmailGitHub@gmail.com>
2025-05-12 10:54:06 +0100

    config

--- config.php
+++ config.php
@@ -1,27 +1,21 @@
 <?php
-class config {
-    private static $pdo = NULL;
-
-    public static function getConnexion() {
-        if (!isset(self::$pdo)) {
-            try {
-                self::$pdo = new PDO(
-                    'mysql:host=localhost;dbname=localoo', 
-                    'root', 
-                    '',
+      class Config {
+        private static $pdo = NULL ; 
+        public static function GetConnexion (){
+            if (!isset(self :: $pdo)){
+                try {
+                    self :: $pdo = new PDO ('mysql:host=localhost;dbname=nour','root','',
                     [
-                        PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
-                        PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
-                        PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8'
-                    ]
-                );
-            } catch(Exception $e) {
-                die('Connection Error: ' . $e->getMessage());
-            }
-        }
-        return self::$pdo;
-    }
-}
+                       PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION, 
+                       PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC 
 
-define('BASE_URL', 'http://localhost/amal/');
+                    ]);
+                } catch (Exception $e){
+                    die('Erreur:'.$e->GetMessage());
+                }
+            }
+            return self ::$pdo ; 
+        }
+   }
+
 ?>
