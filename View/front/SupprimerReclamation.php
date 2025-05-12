ac06a3b3faf5554f8da95990c4c67c4c3df6d52c
VotreNomGitHub <VotreEmailGitHub@gmail.com>
2025-05-12 12:34:17 +0100

    Revert "Merge branch 'main' of https://github.com/KenzaJedidi/ProjetWeb into feature/google-signin"
    This reverts commit ec69e08a7745641637baf5c2f3f5c31b24cc7cb0, reversing
    changes made to d9051ba739d33439285412737c1c73484ff051ea.

--- View/front/SupprimerReclamation.php
+++
@@ -1,8 +0,0 @@
-<?php
-include '../../Controller/ReclamationC.php';
-
-	$message = "" ; 
-	$ReclamationC=new ReclamationC();
-	$ReclamationC->SupprimerReclamation($_GET["idReclamation"]);
-	header('Location:front.php');
-?>
